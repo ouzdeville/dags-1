9a6b8775e1d3e67f4bd83bb30ac72b776c807ec5
cothan <cothanhahaha123@gmail.com>
2017-11-30 01:11:29 -0500

    re format api.h, cycles.h, decapsulation.h, decapsulation.c, fixed typo, no changes to code

--- src/cycles.h
+++ src/cycles.h
@@ -9,11 +9,11 @@
 #define SRC_CYCLES_H_
 
 #include <stdint.h>
-uint64_t rdtsc() {
-	unsigned int lo, hi;
-	__asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
-	return ((uint64_t) hi << 32) | lo;
+uint64_t rdtsc()
+{
+    unsigned int lo, hi;
+    __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+    return ((uint64_t)hi << 32) | lo;
 }
-
 
 #endif /* SRC_CYCLES_H_ */
--- src/decapsulation.h
+++ src/decapsulation.h
@@ -1,14 +1,14 @@
 
-#include<stdlib.h>
-#include<stdio.h>
-#include<sys/types.h>
-#include<stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdint.h>
 #include <assert.h>
 #include <keccak/KangarooTwelve.h>
 
 #include "decoding.h"
 #include "round.h"
-//#include "sponge.h"
+
 #include "fichier.h"
 #include "util.h"
 #define cus_len 4 // I random pick this number to fullfill parameter
@@ -19,7 +19,5 @@
  * @param ct
  * @param sk secret key to extract the key
  */
-int
-decapsulation (unsigned char *ss, const unsigned char *ct,
-	       const unsigned char *sk);
-
+int decapsulation(unsigned char *ss, const unsigned char *ct,
+                  const unsigned char *sk);
